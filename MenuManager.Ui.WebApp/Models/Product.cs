25e8af122076ab7826b1797cbbf2810e04e89a49
tentendcerythmendiable <125271192+tentendcerythmendiable@users.noreply.github.com>
2023-03-27 15:50:44 +0200

    Initial commit

---
+++ MenuManager.Ui.WebApp/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MenuManager.Ui.WebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
